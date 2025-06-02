8fbb44ff2ae1f7e2364d4bc3f627ee53ba216c50
piyu9810 <81683779+piyu9810@users.noreply.github.com>
2025-06-02 18:12:08 +0530

    Add

--- Lists , Hooks , Localstorage , Api Project/src/main.jsx
+++ Lists , Hooks , Localstorage , Api Project/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./store"; // Ensure this file exists
+import store from "./store";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
